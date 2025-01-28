cb1b4110554c6904f4e8ad84f721391090c8dd91
Caonabo Mena <cmena7512@gmail.com>
2025-01-28 18:30:41 -0400

    Add DatabaseModule for PostgreSQL integration and configure global settings

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './Database/database.module';
+import { EntitiesModule } from './Entities/entities.module';
+
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule,  EntitiesModule],
   controllers: [AppController],
   providers: [AppService],
 })
