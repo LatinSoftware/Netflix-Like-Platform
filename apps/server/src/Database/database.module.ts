c4abfac02279620ce471e3955795a1443f200790
Caonabo Mena <cmena7512@gmail.com>
2025-01-28 20:19:34 -0400

    Refactor DatabaseModule to use ConfigModule and update database connection settings

--- apps/server/src/Database/database.module.ts
+++ apps/server/src/Database/database.module.ts
@@ -1,21 +1,25 @@
 import { Module } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
 
 @Module({
     imports: [
         TypeOrmModule.forRootAsync({
-            imports: [ConfigService],
+            imports: [ConfigModule],
             inject: [ConfigService],
             useFactory: (config: ConfigService) => ({
                 type: 'postgres',
                 host: config.get<string>('DB_HOST'),
                 port: config.get<number>('DB_PORT'),
-                username: config.get<string>('DB_USERNAME'),
+                username: config.get<string>('DB_USER'),
                 password: config.get<string>('DB_PASSWORD'),
                 database: config.get<string>('DB_NAME'),
                 entities: [__dirname + '/../Entities/**/*.entity{.ts,.js}'],
+                migrations: [__dirname + '/../Database/migrations/*{.ts,.js}'],
+                namingStrategy: new SnakeNamingStrategy(),
                 synchronize: true,
+                logging: true,
             })
         }),
     ],
