7a4dc534a55b1ae99232db6d464286db0f2b5c34
Caonabo Mena <cmena7512@gmail.com>
2025-02-27 08:00:09 -0400

    Remove CategoryEnum reference from ContentError class

--- apps/server/src/Shared/Messages/Errors/content.error.ts
+++ apps/server/src/Shared/Messages/Errors/content.error.ts
@@ -1,4 +1,4 @@
-import { CategoryEnum } from "src/Entities/Enums/category.enum";
+
 import { DomainException } from "src/Exceptions/DomainException";
 
 export class ContentError{
@@ -10,7 +10,6 @@ export class ContentError{
     public static validAgeRating: DomainException = new DomainException("Content.validAgeRating", "The age rating must be a valid number");
     public static validOriginalLanguage: DomainException = new DomainException("Content.validOriginalLanguage", "The original language must be a valid string");
     public static validPopularity: DomainException = new DomainException("Content.validPopularity", "The popularity must be a valid number");
-    public static validCategory: DomainException = new DomainException("Content.validCategory", `The category must be one of the following values: ${Object.values(CategoryEnum).join(", ")}`);
     public static validGenres: DomainException = new DomainException("Content.validGenres", "At least one genre is required");
     public static genreAlreadyAssociated: DomainException = new DomainException("Content.genreAlreadyAssociated", "The genre is already associated with the content");
     public static invalidPopularity: DomainException = new DomainException("Content.invalidPopularity", "The popularity must be a number between 0 and 100");
