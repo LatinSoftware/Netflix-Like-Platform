ea5adf3c4834572bbb8f9ef14bff77edda33d2f2
Caonabo Mena <cmena7512@gmail.com>
2025-02-27 08:00:04 -0400

    Add Content and Category modules to the application module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -6,6 +6,8 @@ import { DatabaseModule } from './Database/database.module';
 import { EntitiesModule } from './Entities/entities.module';
 import { GenreModule } from './feature/genre/genre.module';
 import { CqrsModule } from '@nestjs/cqrs';
+import { ContentModule } from './feature/content/content.module';
+import { CategoryModule } from './feature/category/category.module';
 
 @Module({
   imports: [
@@ -14,6 +16,8 @@ import { CqrsModule } from '@nestjs/cqrs';
     DatabaseModule,
     EntitiesModule,
     GenreModule,
+    ContentModule,
+    CategoryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
